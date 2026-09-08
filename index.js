503b849778464fea4cc65754fdf84e4144aadc7b
LenaFedchenko <lenafedcenko98@gmail.com>
2026-09-08 13:41:15 +0300

    l

---
+++ index.js
@@ -0,0 +1,16 @@
+
+import express from 'express'
+import moment from 'moment';
+
+moment().format();
+const app = express()
+
+app.get('/timestamp', (req, res) => {
+  res.json({
+    "date": moment().format('YYYY-MM-DD')
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
