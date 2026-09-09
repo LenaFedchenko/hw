6de0c661e437dd9756117fc9562fedc163a15bf1
LenaFedchenko <lenafedcenko98@gmail.com>
2026-09-09 22:22:36 +0300

    hw2

--- index.js
+++ index.js
@@ -6,11 +6,23 @@ moment().format();
 const app = express()
 
 app.get('/timestamp', (req, res) => {
-  res.json({
-    "date": moment().format('YYYY-MM-DD')
-  })
+    res.json({
+        "date": moment().format('YYYY-MM-DD')
+    })
+})
+app.get('/health', (req, res) => {
+    res.json({
+        "status": "ok"
+    })
+})
+app.get('/stats', (req, res) => {
+    res.json({
+        "uptime": process.uptime(),
+        "nodeVersion": process.versions.node,
+        "timestamp": moment().format('YYYY-MM-DD')
+    })
 })
 
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
+    console.log('Server is running on http://localhost:3000')
 })
